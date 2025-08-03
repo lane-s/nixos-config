d5ce68adb3a9690897d9c1cbfff23ab56f484055
Lane Spangler <las4vc@virginia.edu>
2025-08-03 15:20:09 +0200

    Fix hardware config

--- hosts/lsp-desktop/hardware-configuration.nix
+++ hosts/lsp-desktop/hardware-configuration.nix
@@ -7,20 +7,15 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   # Kernel modules
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = ["ext4" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ]; # i5-4690K uses Intel VT-x
   boot.extraModulePackages = [ ];
 
   # Filesystems - REPLACE WITH YOUR ACTUAL CONFIGURATION
   fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
+    device = "/dev/sdb2";
     fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
   };
 
   swapDevices = [ ];
